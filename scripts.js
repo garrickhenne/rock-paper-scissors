261c0fb21670b99c69f57da6a9056bd58bf5d134
Garrick Henne <garrickhenne@outlook.com>
2021-05-15 16:06:26 -0700

    Added simple ui that keeps track of scores, adds buttons that track your choice, and text for decisions made

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -23,22 +24,28 @@ function playRound(playerSelection, computerSelection) {
             if (playerSelectionLower === "rock") {
                 return "Its a Tie! rock and rock";
             } else if (playerSelectionLower === "paper") {
+                playerWin();
                 return "You Win! Paper beats rock"; 
             } else {
+                compWin();
                 return "You Lose! Rock beats scissors";
             }
         case "paper":
             if (playerSelectionLower === "rock") {
+                compWin();
                 return "You Lose! Paper beats rock";
             } else if (playerSelectionLower === "paper") {
                 return "its a Tie! paper and paper"; 
             } else {
+                playerWin();
                 return "You Win! Scissors beats paper";
             }
         case "scissors":
             if (playerSelectionLower === "rock") {
+                playerWin();
                 return "You Win! Rock beats scissors";
             } else if (playerSelectionLower === "paper") {
+                compWin();
                 return "You Lose! Scissors beats paper"; 
             } else {
                 return "Its a Tie! scissors and scissors";
@@ -49,34 +56,68 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-
-        let playerChoice = prompt("rock, paper, or scissors?");
-
-        let decision = playRound(playerChoice, computerPlay());
-
-        if (decision.includes("Win")) {
-            playerScore++;
-            console.log(decision);
-        } else if (decision.includes("Lose")) {
-            computerScore++;
-            console.log(decision);
-        } else if (decision.includes("Tie")) {
-            console.log(decision);
-        } else {
-            console.log(decision);
+const buttons = document.querySelectorAll('button');
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.textContent.includes("Rock")) {
+            decisionVar.textContent = playRound("rock", computerPlay());
+            checkScores();
+        } else if (btn.textContent.includes("Paper")) {
+            decisionVar.textContent = playRound("paper", computerPlay());
+            checkScores();
+        } else if (btn.textContent.includes("Scissors")) {
+            decisionVar.textContent = playRound("scissors", computerPlay());
+            checkScores();
+        } else if (btn.textContent.includes("Reset Score")) {
+            resetScores();
         }
-    }
+    });
+});
 
-    if (playerScore > computerScore) {
-        return "Congratulations, you won the series! " + playerScore + " to " + computerScore;
-    } else if (computerScore > playerScore) {
-        return "Sorry, you lost the series! " + computerScore + " to " + playerScore;
-    } else {
-        return "You tied up! " + playerScore + " to " + computerScore;
+const pScoreClass = document.querySelector('.player-score');
+
+let pScoreVar = document.createElement('h1');
+pScoreVar.textContent = 0;
+
+let cScoreVar = document.createElement('h1');
+cScoreVar.textContent = 0;
+
+pScoreClass.appendChild(pScoreVar);
+
+const cScoreClass = document.querySelector('.comp-score');
+cScoreClass.appendChild(cScoreVar);
+
+const decisionClass = document.querySelector('.decision');
+let decisionVar = document.createElement('h3');
+decisionClass.appendChild(decisionVar);
+
+function playerWin() {
+    pScoreVar.textContent = parseInt(pScoreVar.textContent) + 1;
+}
+
+function compWin() {
+    cScoreVar.textContent = parseInt(cScoreVar.textContent) + 1;
+
+    if (cScoreVar.textContent == 5) {
+        decisionVar.textContent = "Sorry! You lost the series...";
     }
 }
+
+function resetScores() {
+    pScoreVar.textContent = 0;
+    cScoreVar.textContent = 0;
+    decisionVar.textContent = "";
+    decisionVar.style.color = 'black';
+}
+
+function checkScores() {
+    if (pScoreVar.textContent == 5) {
+        decisionVar.style.color = 'green';
+        decisionVar.textContent = "Congratulations! You won the series!";
+    } else if (cScoreVar.textContent == 5) {
+        decisionVar.style.color = 'red';
+        decisionVar.textContent = "Sorry! You lost the series...";
+    }
+}
+
+
